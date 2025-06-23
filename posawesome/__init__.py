e3cfe56075973038d3142ad7f0f791a274a00a83
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-06-23 09:37:59 +0200

    Update __init__.py

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
-
-__version__ = "6.3.0"
-
-
-def console(*data):
-    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+try:
+    import frappe
+    
+    __version__ = "6.3.0"
+    
+    
+    def console(*data):
+        frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+    
