c463cc2a506e7d8cd9ec7b77e02d8475214a765b
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-06-23 09:39:32 +0200

    fix init

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 try:
     import frappe
-    
+
     __version__ = "6.3.0"
-    
-    
+
     def console(*data):
         frappe.publish_realtime("toconsole", data, user=frappe.session.user)
-    
+
+except ImportError:
+    # Optionally handle the error or just pass
+    pass
